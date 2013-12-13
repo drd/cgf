11d73bb77277bc5a8a004329cdcfab390be0f262
Eric O'Connell <eric.oconnell@idealist.org>
2013-12-12 20:56:44 -0800

    add grunt-contrib-connect & server task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,22 @@ module.exports = function(grunt) {
                 src: 'src/playground.js',
                 dest: 'www/index.js'
             }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: 'www',
+                    keepalive: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     grunt.registerTask('default', ['browserify']);
+    grunt.registerTask('server', ['browserify', 'connect']);
 };
